872dbb3dc72a3ae497cc72fc470ea787f2d6b17a
tomasnovacek9 <tomas.nova9@seznam.cz>
2026-05-30 17:07:53 +0200

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,3 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e => { e.respondWith(fetch(e.request)); });
+self.addEventListener('install', event => { self.skipWaiting(); });
+self.addEventListener('activate', event => { event.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', event => { event.respondWith(fetch(event.request)); });
